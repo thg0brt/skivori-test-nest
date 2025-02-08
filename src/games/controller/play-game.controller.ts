f2ed6a2977249af449dfc4ca331272e98cb7200f
Thiago Brito <thiago.13.brito@gmail.com>
2025-02-07 21:00:21 -0300

    play API endpoint working and other improvement

--- src/games/controller/play-game.controller.ts
+++ src/games/controller/play-game.controller.ts
@@ -1,12 +1,83 @@
 import { Controller, Get, Param } from '@nestjs/common';
-import gameData from '../data/game-data.json'; //imports game-data.json
 
 @Controller('games') //default route for game requests
 export class PlayGame {
 
-    @Get('play-game/:id/:coins') 
-    async playGame(@Param('id') id : String, @Param('coins') startCoins : String) {
+    @Get('play') 
+    async playGame() {
 
+        //Define the machine slot reels
+        const reel1 = ["cherry", "lemon", "apple", "lemon", "banana", "banana", "lemon", "lemon"];
+        const reel2 = ["lemon",  "apple", "lemon", "lemon", "cherry", "apple",  "banana", "lemon"];
+        const reel3 = ["lemon",  "apple", "lemon", "apple", "cherry", "lemon",  "banana", "lemon"];
+
+        //Get the random key for each reel.
+        const randomKey1 = Math.floor(Math.random() * 8);
+        const randomKey2 = Math.floor(Math.random() * 8);
+        const randomKey3 = Math.floor(Math.random() * 8);
+
+        //create the array with the results
+        const results = [reel1[randomKey1], reel2[randomKey2],  reel3[randomKey3]];
+
+        
+        var count = 0;
+        var lastFruit = "";
+        results.forEach((fruit) => {
+            if(count == 0){
+                lastFruit = fruit;
+                count++;
+            }else{
+                if(fruit == lastFruit){
+                    count++;
+                }
+
+                if(count < 2){
+                    lastFruit = fruit;
+                    count = 1;
+                }
+            }
+        });
+
+        var coinsEarned = 0;
+
+        //calculate the points earned/lost
+        if (count == 2){
+            switch(lastFruit){
+                case 'cherry':
+                    coinsEarned = 40;
+                    break;
+                case 'apple':
+                    coinsEarned = 10;
+                    break;
+                case 'banana':
+                    coinsEarned = 5;
+                    break;
+                case 'lemon':
+                    coinsEarned = 1; //the test description dont specify the points earned for 2 lemons, but i defined it as 1;
+                    break;
+            }
+
+        } else if (count == 3) {
+            switch(lastFruit){
+                case 'cherry':
+                    coinsEarned = 50;
+                    break;
+                case 'apple':
+                    coinsEarned = 20;
+                    break;
+                case 'banana':
+                    coinsEarned = 15;
+                    break;
+                case 'lemon':
+                    coinsEarned = 3;
+                    break;
+            }
+        }else{
+            coinsEarned = -1; //Each spin costs 1 coin.
+        }
+        
+        return [coinsEarned, results];
     }
+
 
 }
--- src/games/controller/search-games.controller.ts
+++ src/games/controller/search-games.controller.ts
@@ -4,10 +4,9 @@ import gameData from '../data/game-data.json'; //imports game-data.json
 @Controller('games') //default route for game requests
 export class SearchGames {
 
-    @Get('search-games/:search') //GET endpoint Route for getting all games in the game-data.json
-    async getGames(@Param('search') search : String) {
-        const filteredGame = gameData.filter((game) => game.title.toLowerCase().indexOf(search.toLowerCase()) > -1 )
-        return filteredGame; 
+    @Get('search-games/:search') //GET endpoint Route for searching games in the game-data.json
+    async searchGames(@Param('search') search : String) {
+        return gameData.filter((game) => game.title.toLowerCase().indexOf(search.toLowerCase()) > -1 ) //searches the passed param in title of json.
     }
 
 }
