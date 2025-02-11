04bd5005a276ed58ec8e0c6a90fb37feb975921a
Thiago Brito <thiago.13.brito@gmail.com>
2025-02-10 23:00:46 -0300

    cors configuration

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,12 @@ async function bootstrap() {
 
     // Enable CORS
     app.enableCors({
-      origin: ['http://localhost:3000', 'http://localhost:3001'],  // Permitir origens específicas
+      origin: [
+        'http://localhost:3000',
+        'http://localhost:3001',
+        "http://192.168.100.103:3001", //my local network I use with my phone
+        "https://skivori-test-next.vercel.app", //deploy front-end cloud service
+        ],
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
       credentials: true,
     });
