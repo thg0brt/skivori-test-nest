df63b6dd692f03c8bf4d8b979369961711f5887d
Thiago Brito <thiago.13.brito@gmail.com>
2025-02-12 04:46:09 -0300

    changed comment

--- src/games/controller/search-games.controller.ts
+++ src/games/controller/search-games.controller.ts
@@ -4,7 +4,7 @@ import gameData from '../data/game-data.json'; //imports game-data.json
 @Controller('games') //default route for game requests
 export class SearchGames {
 
-    @Get('search-games/:search') // GET endpoint to search for games in game-data.json
+    @Get('search-games/:search') // GET endpoint to search games by title from game-data.json
     async searchGames(@Param('search') search : String) {
         return gameData.filter((game) => game.title.toLowerCase().indexOf(search.toLowerCase()) > -1 ) //searches in the game-data.json for the passed param.
     }
